7ef64bb1bb9fbd12aeb1dbe5383dca36bba91e18
Eduardo <ealvarez@carminaticonsulting.com>
2023-11-18 14:39:36 -0600

    added proxy server to communicate with hardcoded api

--- PizzaClient/src/components/Pizza.jsx
+++ PizzaClient/src/components/Pizza.jsx
@@ -2,57 +2,62 @@ import { useState, useEffect } from "react";
 import PizzaList from "./PizzaList";
 
 const term = "Pizza";
+const API_URL = "/pizzas";
+const headers = {
+  "Content-Type": "application/json",
+};
 
 function Pizza() {
   const [data, setData] = useState([]);
-  const [maxId, setMaxId] = useState(0);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetchPizzaData();
   }, []);
 
   const fetchPizzaData = () => {
-    // Simulate fetching data from API
-    const pizzaData = [
-      {
-        id: 1,
-        name: "Margherita",
-        description: "Tomato sauce, mozzarella, and basil",
-      },
-      {
-        id: 2,
-        name: "Pepperoni",
-        description: "Tomato sauce, mozzarella, and pepperoni",
-      },
-      {
-        id: 3,
-        name: "Hawaiian",
-        description: "Tomato sauce, mozzarella, ham, and pineapple",
-      },
-    ];
-    setData(pizzaData);
-    setMaxId(Math.max(...pizzaData.map((pizza) => pizza.id)));
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => setError(error));
   };
 
   const handleCreate = (item) => {
-    // Simulate creating item on API
-    const newItem = { ...item, id: data.length + 1 };
-    setData([...data, newItem]);
-    setMaxId(maxId + 1);
+    console.log(`add item: ${JSON.stringify(item)}`);
+
+    fetch(API_URL, {
+      method: "POST",
+      headers,
+      body: JSON.stringify({ name: item.name, description: item.description }),
+    })
+      .then((response) => response.json())
+      .then((returnedItem) => setData([...data, returnedItem]))
+      .catch((error) => setError(error));
   };
 
-  const handleUpdate = (item) => {
-    // Simulate updating item on API
-    const updatedData = data.map((pizza) =>
-      pizza.id === item.id ? item : pizza
-    );
-    setData(updatedData);
+  const handleUpdate = (updatedItem) => {
+    console.log(`update item: ${JSON.stringify(updatedItem)}`);
+
+    fetch(`${API_URL}/${updatedItem.id}`, {
+      method: "PUT",
+      headers,
+      body: JSON.stringify(updatedItem),
+    })
+      .then(() =>
+        setData(
+          data.map((item) => (item.id === updatedItem.id ? updatedItem : item))
+        )
+      )
+      .catch((error) => setError(error));
   };
 
   const handleDelete = (id) => {
-    // Simulate deleting item on API
-    const updatedData = data.filter((pizza) => pizza.id !== id);
-    setData(updatedData);
+    fetch(`${API_URL}/${id}`, {
+      method: "DELETE",
+      headers,
+    })
+      .then(() => setData(data.filter((item) => item.id !== id)))
+      .catch((error) => console.error("Error deleting item:", error));
   };
 
   return (
@@ -60,6 +65,7 @@ function Pizza() {
       <PizzaList
         name={term}
         data={data}
+        error={error}
         onCreate={handleCreate}
         onUpdate={handleUpdate}
         onDelete={handleDelete}
